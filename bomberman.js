46a7894c5a6808959aa875792fc7803e4a8fcc84
Edwin Andermyr <andermyredwin@gmail.com>
2023-12-12 16:22:53 +0100

    generating players in random position

--- bomberman.js
+++ bomberman.js
@@ -1,13 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
   createGameBoard();
 });
+// Declaring variables
+const gameContainer = document.querySelector(".game-container");
+const rowLabels = ["A", "B", "C", "D", "E", "F", "G"];
+const colLabels = ["1", "2", "3", "4", "5", "6", "7"];
 
 function createGameBoard() {
-  const gameContainer = document.getElementById("game-container");
-
-  const rowLabels = ["A", "B", "C", "D", "E", "F", "G"];
-  const colLabels = ["1", "2", "3", "4", "5", "6", "7"];
-
   for (let row = 0; row < 7; row++) {
     for (let col = 0; col < 7; col++) {
       const gridSquare = document.createElement("div");
@@ -17,3 +16,12 @@ function createGameBoard() {
     }
   }
 }
+
+const generatePlayer1 = () => {
+  let playerRow = rowLabels[Math.floor(Math.random() * rowLabels.length)];
+  let playerCol = colLabels[Math.floor(Math.random() * colLabels.length)];
+  playerPosition = `${playerRow}${playerCol}`;
+  console.log(playerPosition);
+};
+
+generatePlayer1();
