4310a7e06e4c3fb7f948c8b861100201690307a7
Edwin Andermyr <andermyredwin@gmail.com>
2023-12-14 14:15:58 +0100

    movement functions for right and left done

--- bomberman.js
+++ bomberman.js
@@ -1,11 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
   createGameBoard();
   generatePlayer1();
+  moveLeft();
+  moveRight();
 });
-// Declaring variables
+
 const gameContainer = document.querySelector(".game-container");
 const rowLabels = ["A", "B", "C", "D", "E", "F", "G"];
 const colLabels = ["1", "2", "3", "4", "5", "6", "7"];
+let playerRow = rowLabels[Math.floor(Math.random() * rowLabels.length)];
+let playerCol = colLabels[Math.floor(Math.random() * colLabels.length)];
+let playerXY = `${playerRow}${playerCol}`;
 
 function createGameBoard() {
   for (let row = 0; row < 7; row++) {
@@ -19,10 +24,6 @@ function createGameBoard() {
 }
 
 const generatePlayer1 = () => {
-  let playerRow = rowLabels[Math.floor(Math.random() * rowLabels.length)];
-  let playerCol = colLabels[Math.floor(Math.random() * colLabels.length)];
-  let playerXY = `${playerRow}${playerCol}`;
-
   let playerDiv = document.querySelector(`.grid-square.${playerXY}`);
 
   if (playerDiv) {
@@ -33,4 +34,66 @@ const generatePlayer1 = () => {
   } else {
     console.error(`Element with class .grid-square.${playerXY} not found.`);
   }
+};
+// mouvement part
+const moveLeft = () => {
+  document.addEventListener("keydown", (event) => {
+    if (event.key === "ArrowLeft") {
+      console.log("ArrowLeft key pressed!");
+
+      // Check if moving left is within the game board boundaries
+      if (playerCol > 1) {
+        let playerDiv = document.querySelector(`.grid-square.${playerXY}`);
+        playerDiv.innerHTML = "";
+
+        playerCol--; // Update the player's column position
+
+        let leftXY = `${playerRow}${playerCol}`;
+        let leftDiv = document.querySelector(`.grid-square.${leftXY}`);
+
+        if (leftDiv) {
+          const playerImage = document.createElement("img");
+          playerImage.src = "./images/bomberman1.jpeg";
+          playerImage.classList.add("player-image");
+          leftDiv.appendChild(playerImage);
+          playerXY = leftXY;
+        } else {
+          console.log("We cannot move there");
+        }
+      } else {
+        console.log("Cannot move left, at the edge of the board.");
+      }
+    }
+  });
+};
+
+const moveRight = () => {
+  document.addEventListener("keydown", (event) => {
+    if (event.key === "ArrowRight") {
+      console.log("ArrowRight key pressed!");
+
+      // Check if moving left is within the game board boundaries
+      if (playerCol < 7) {
+        let playerDiv = document.querySelector(`.grid-square.${playerXY}`);
+        playerDiv.innerHTML = "";
+
+        playerCol++;
+
+        let RightXY = `${playerRow}${playerCol}`;
+        let RightDiv = document.querySelector(`.grid-square.${RightXY}`);
+
+        if (RightDiv) {
+          const playerImage = document.createElement("img");
+          playerImage.src = "./images/bomberman1.jpeg";
+          playerImage.classList.add("player-image");
+          RightDiv.appendChild(playerImage);
+          playerXY = RightXY;
+        } else {
+          console.log("We cannot move there");
+        }
+      } else {
+        console.log("Cannot move right, at the edge of the board.");
+      }
+    }
+  });
 };
